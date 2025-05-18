9bd305c0db6e33357e2fcbd5427eaa42983c4d87
Franco <francograndedela@gmail.com>
2025-05-18 19:49:51 -0300

    asdasdsad

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -8,25 +8,32 @@ export const createOrder = async (req, res) => {
     const {userId, cartId} = req.body;
 
     try {
+        let total = 0;
         const cart = await cartModel.findById(cartId).populate("products.product");
         if(!cart) {
+            console.log("Carrito no encontrado");
             return res.status(404).send({status: "error", error: "Carrito no encontrado"});
         }
 
-        let total = 0;
+        console.log("Productos en el carrito:", cart.products);
+        
 
         for (const item of cart.products) {
-            const product = await productModel.findById(item.product._id);
-            if (!product) {
-                return res.status(400).send({status: "error", error: "Producto no disponible"});
+            if (!item.product) continue;
+        
+            if (item.product.stock < item.quantity) {
+            return res.status(400).send({ error: `Sin stock para ${item.product.nombre}` });
             }
-            if (item.quantity > product.stock) {
-                return res.status(400).send({status: "error", error: "Stock insuficiente"});
-            }
-            product.stock -= item.quantity;
-            total += product.price * item.quantity;
-            await product.save();
+        
+            item.product.stock -= item.quantity;
+            await item.product.save();
+        
+            total += item.product.precio * item.quantity;
         }
+
+        const order = await orderService.createOrder(userId, cartId, total);
+        console.log("Pedido creado con éxito:", order);
+        res.send({status: "success", message: "Pedido creado con éxito"});
         
     } catch (error) {
         res.status(500).send({status: "error", error: error.message});
--- src/dao/dto/order.dto.js
+++ src/dao/dto/order.dto.js
@@ -1,8 +1,9 @@
 export default class OrderDTO{
     constructor(order){
+        this.id = order._id;
             this.user = order.user;
             this.cart = order.cart;
-            this.total = total.total;
+            this.total = order.total;
             this.createdAt = order.createdAt;
     }
 }
