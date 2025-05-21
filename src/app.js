268dc3266f936c10c444d8724739b4eea004d6bb
Franco <francograndedela@gmail.com>
2025-05-21 13:23:38 -0300

    modificando rutas en app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import config from './config/config.js';
-import userRouter from './routes/user.router.js';
-import orderRouter from './routes/order.router.js';
-import cartRouter from './routes/cart.router.js';
+import UserRouter from './routes/user.router.js';
+import OrderRouter from './routes/order.router.js';
+import CartRouter from './routes/cart.router.js';
 import ProductRouter from './routes/product.router.js';
 import connectDB from './config/db.config.js'; 
 import dictionaryRouter from './routes/dictionary.router.js';
@@ -19,10 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 const connection = connectDB(config.MONGO_URL);
 
 // routers
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
+app.use('/api/users', new UserRouter().getRouter());
+app.use('/api/orders', new OrderRouter().getRouter());
 app.use('/api/products', new ProductRouter().getRouter());
-app.use('/api/carts', cartRouter);
+app.use('/api/carts', new CartRouter().getRouter());
 
 app.use('/api/dictionary', dictionaryRouter);
 app.use('/api/session', (new SessionRouter()).getRouter());
